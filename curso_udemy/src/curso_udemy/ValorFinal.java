1dedc386b0cf47a9766d6f6e2d87515492eb9185
MariaHelenaLB <mariahelenalimabarreto13@gmail.com>
2024-05-23 13:45:57 -0300

    Expressão condicional ternária.

---
+++ curso_udemy/src/curso_udemy/ValorFinal.java
@@ -0,0 +1,32 @@
+package curso_udemy;
+
+import java.util.Scanner;
+
+public class ValorFinal {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		double preco = 34.5;
+		double desconto = (preco < 20.0) ? preco * 0.1 : preco * 0.05;
+		
+		/*
+		FORMA MAIS COMPLICADA DE FAZER;
+		
+		double preco = 34.5;
+		double desconto;
+		
+		if (preco < 20.0) {
+			desconto = preco * 0.1;
+		} else {
+			desconto = preco * 0.05;
+		}
+		*/
+		
+		System.out.println(desconto);
+		sc.close();
+		
+	}
+
+} 
